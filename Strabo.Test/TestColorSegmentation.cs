a68a2d2efed7efffa7f3f5f5a000421ebdb352b3
Yao-Yi Chiang <yaoyichi@gmail.com>
2014-01-10 19:36:35 -0800

    Update

---
+++ Strabo.Test/TestColorSegmentation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Strabo.Core.ColorSegmentation;
+
+namespace Strabo.Test
+{
+    public class TestColorSegmentation
+    {
+        public static void test()
+        {
+            string dir=@"..\..\data\";
+            string fn = "lena.jpg";
+
+            ColorSegmentationWorker csw = new ColorSegmentationWorker();
+            csw.Apply(dir, fn);
+        }
+    }
+}
---
+++ Strabo.Test/TestTextIdentification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Strabo.Core.TextDetection;
+
+namespace Strabo.Test
+{
+    public class TestTextIdentification
+    {
+        public static void test()
+        {
+            string dir=@"..\..\data\";
+            string fn = "text.png";
+            TextDetectionWorker trw = new TextDetectionWorker();
+            trw.Apply(dir, fn, 2.5, false);
+        }
+    }
+}
