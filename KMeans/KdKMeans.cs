53a8ac5686c84416528568702f1d90829a50d508
ramtinb <ramtinb@gmail.com>
2014-05-21 21:31:36 -0700

    fix KdkMeans

--- KMeans/KdKMeans.cs
+++ KMeans/KdKMeans.cs
@@ -6,12 +6,13 @@ namespace KdKeys.DataMining.Clustering.KMeans
 {
 
 	/// <summary>
-	/// This class implement a KMeans clustering algorithm
+	/// This class implement a KMeans clustering algorithm old
 	/// </summary>
 	public class KdKMeans
 	{
-		public KdKMeans()
+        public KdKMeans() 
 		{
+
 		}
 		
 		/// <summary>
